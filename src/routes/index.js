7e031d4a5f384de5a248663783208581c6bba1e8
MarlonMarques <marlon.belohd@gmail.com>
2020-07-19 17:44:28 -0400

    added screen login and routes
    added screen login and routes

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from '../screens/LoginScreen';
+
+const Stack = createStackNavigator();
+
+export default function Navigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Block, Text } from '../elements';
+
+export default function LoginScreen() {
+  return (
+    // eslint-disable-next-line no-use-before-define
+    <Block middle center>
+      <Text>Burger Monster Delivery</Text>
+    </Block>
+  );
+}
